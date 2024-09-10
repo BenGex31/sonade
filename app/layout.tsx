866c7c786499b904e489e1e3cc17f966c1e167ba
Benjamin Desole <benjamin.desole@gmail.com>
2024-09-10 11:40:12 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+//import "./globals.css";
+import "@mantine/core/styles.css";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 
 const geistSans = localFont({
