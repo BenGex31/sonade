ccedf3dca00a250fb6f3b69e329e9afd924b5412
Benjamin Desole <benjamin.desole@gmail.com>
2024-09-10 18:13:07 +0200

    Update definitions.ts

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,6 +1,14 @@
 export type Loan = {
-    amount: number;
-    duration: number;
-    rate: number;
-    insurance: number
-}
+  amount: number;
+  duration: number;
+  rate: number;
+  insurance: number;
+};
+
+export type LoanTableRow = {
+  year: number;
+  annuity: number;
+  interest: number;
+  insurance: number;
+  remainingCapital: number;
+};
