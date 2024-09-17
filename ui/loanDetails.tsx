5cef929513645a6c8e5ab20359d6e4b2ab6ef4ff
Benjamin Desole <benjamin.desole@gmail.com>
2024-09-17 21:57:36 +0200

    Update loanDetails.tsx

--- ui/loanDetails.tsx
+++ ui/loanDetails.tsx
@@ -22,20 +22,34 @@ export default function Loandetails({
   return (
     <Stack w={350} gap={"xl"}>
       <Stack gap={"2px"}>
-        <Title size={"h3"}>Mensualités: {totalMonthlyPayments} € / mois</Title>
+        <Title size={"h3"}>
+          Mensualités: {totalMonthlyPayments.toLocaleString("fr-FR")} € / mois
+        </Title>
         <List withPadding>
-          <List.Item>dont Capital moyen: {averageCapital} € / mois</List.Item>
           <List.Item>
-            dont Intérêts moyens: {monthlyInterests} € / mois
+            dont Capital moyen: {averageCapital.toLocaleString("fr-FR")} € /
+            mois
           </List.Item>
-          <List.Item>dont Assurance: {monthlyInsurance} € / mois</List.Item>
+          <List.Item>
+            dont Intérêts moyens: {monthlyInterests.toLocaleString("fr-FR")} € /
+            mois
+          </List.Item>
+          <List.Item>
+            dont Assurance: {monthlyInsurance.toLocaleString("fr-FR")} € / mois
+          </List.Item>
         </List>
       </Stack>
       <Stack gap={"2px"}>
-        <Title size={"h3"}>Cout total: {totalCostAmount} €</Title>
+        <Title size={"h3"}>
+          Cout total: {totalCostAmount.toLocaleString("fr-FR")} €
+        </Title>
         <List withPadding>
-          <List.Item>dont Intérêts: {totalCostInterests} €</List.Item>
-          <List.Item>dont Assurance: {totalInsuranceCost} €</List.Item>
+          <List.Item>
+            dont Intérêts: {totalCostInterests.toLocaleString("fr-FR")} €
+          </List.Item>
+          <List.Item>
+            dont Assurance: {totalInsuranceCost.toLocaleString("fr-FR")} €
+          </List.Item>
         </List>
       </Stack>
     </Stack>
